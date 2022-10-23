b1e284fc01c0dee0f84f2edd365722b9aba8d164
Gabi <gabrielle.rof@gmail.com>
2022-10-23 16:22:39 -0300

    Tela de Baixa de Parcelas

--- ubaixaparcela.pas
+++ ubaixaparcela.pas
@@ -14,14 +14,13 @@ type
 
   TFRMBaixaParcela = class(TForm)
     DateEdit1: TDateEdit;
-    DateEdit2: TDateEdit;
-    Edit1: TEdit;
-    Edit2: TEdit;
-    Edit3: TEdit;
-    Edit5: TEdit;
-    Edit6: TEdit;
-    Edit7: TEdit;
-    Label1: TLabel;
+    DtaVencimento: TDateEdit;
+    EdtValorParcela: TEdit;
+    EdtContaBank: TEdit;
+    EdtFormPg: TEdit;
+    EdtMulta: TEdit;
+    EdtDesconto: TEdit;
+    Label10: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
@@ -29,23 +28,21 @@ type
     Label6: TLabel;
     Label7: TLabel;
     Label8: TLabel;
-    Label9: TLabel;
+    LblTotPg: TLabel;
     Panel1: TPanel;
-    Panel10: TPanel;
     Panel2: TPanel;
     Panel3: TPanel;
     Panel4: TPanel;
     Panel5: TPanel;
     Panel6: TPanel;
-    Panel7: TPanel;
     Panel8: TPanel;
     Panel9: TPanel;
-    SpeedButton1: TSpeedButton;
-    SpeedButton2: TSpeedButton;
-    SpeedButton3: TSpeedButton;
-    SpeedButton4: TSpeedButton;
-    SpeedButton5: TSpeedButton;
-    procedure DateEdit2Change(Sender: TObject);
+    BtnConsFormPg: TSpeedButton;
+    BtnConsConta: TSpeedButton;
+    BtnConfirma: TSpeedButton;
+    BtnSair: TSpeedButton;
+    BtnCancela: TSpeedButton;
+    procedure DtaVencimentoChange(Sender: TObject);
     procedure Panel2Click(Sender: TObject);
   private
 
@@ -67,7 +64,7 @@ begin
 
 end;
 
-procedure TFRMBaixaParcela.DateEdit2Change(Sender: TObject);
+procedure TFRMBaixaParcela.DtaVencimentoChange(Sender: TObject);
 begin
 
 end;
